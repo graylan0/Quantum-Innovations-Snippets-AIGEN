b6127fc628f94e82a3d454a483fd76035c04716c
Graylan <34530588+graylan0@users.noreply.github.com>
2023-11-18 05:55:58 -0500

    Update quantum.vectorizer.py

--- critical_hot_logs/quantum.vectorizer.py
+++ critical_hot_logs/quantum.vectorizer.py
@@ -8,8 +8,16 @@ import pennylane as qml
 from pennylane import numpy as np
 from textblob import TextBlob
 from llama_cpp import Llama
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 dev = qml.device("default.qubit", wires=4)
+llm = Llama(
+  model_path="llama-2-7b-chat.ggmlv3.q8_0.bin",
+  n_gpu_layers=-1,
+  n_ctx=3900,
+)
 
 @qml.qnode(dev)
 def quantum_circuit(color_code, amplitude):
@@ -28,31 +36,35 @@ def sentiment_to_amplitude(text):
     analysis = TextBlob(text)
     return (analysis.sentiment.polarity + 1) / 2
 
-llm = Llama(model_path="./models/7B/llama-model.gguf")
-
 async def llama_generate_and_colorize(session, prompt, max_tokens=3900, chunk_size=980):
     prompt_chunks = [prompt[i:i + chunk_size] for i in range(0, len(prompt), chunk_size)]
     generated_text = ""
     color_code = "#000000"
     for chunk in prompt_chunks:
-        output = llm(chunk, max_tokens=min(max_tokens, chunk_size), stop=["Q:", "\n"], echo=True)
-        text = output.get("choices", [{}])[0].get("text", "")
-        generated_text += text
-        color_code = re.findall(r'#[0-9A-Fa-f]{6}', text)[-1] if re.findall(r'#[0-9A-Fa-f]{6}', text) else color_code
+        try:
+            output = llm(chunk, max_tokens=min(max_tokens, chunk_size), stop=["Q:", "\n"], echo=True)
+            text = output.get("choices", [{}])[0].get("text", "")
+            generated_text += text
+            color_code = re.findall(r'#[0-9A-Fa-f]{6}', text)[-1] if re.findall(r'#[0-9A-Fa-f]{6}', text) else color_code
+        except Exception as e:
+            logging.error(f"Error in llama_generate_and_colorize: {e}")
     return generated_text, color_code
 
 async def send_batch_async(batch, batch_number, session, weaviate_url):
     start_time = time.time()
     tasks = []
     for item in batch:
-        if 'id' in item:
-            item['message_id'] = item.pop('id')
-        if 'mapping' in item and isinstance(item['mapping'], dict):
-            item['mapping'] = json.dumps(item['mapping'])
-        if 'moderation_results' in item and isinstance(item['moderation_results'], list):
-            item['moderation_results'] = json.dumps(item['moderation_results'])
-        url = f"{weaviate_url}/objects"
-        tasks.append(session.post(url, json={"class": "ChatGPTHistory", "properties": item}))
+        try:
+            if 'id' in item:
+                item['message_id'] = item.pop('id')
+            if 'mapping' in item and isinstance(item['mapping'], dict):
+                item['mapping'] = json.dumps(item['mapping'])
+            if 'moderation_results' in item and isinstance(item['moderation_results'], list):
+                item['moderation_results'] = json.dumps(item['moderation_results'])
+            url = f"{weaviate_url}/objects"
+            tasks.append(session.post(url, json={"class": "ChatGPTHistory", "properties": item}))
+        except Exception as e:
+            logging.error(f"Error in send_batch_async: {e}")
     responses = await asyncio.gather(*tasks)
     end_time = time.time()
 
@@ -68,13 +80,16 @@ async def main():
             current_batch = []
             current_char_count = 0
             while current_char_count < max_chars_per_batch and data:
-                item = random.choice(data)
-                data.remove(item)
-                item_json = json.dumps(item)
-                item_size = len(item_json)
-                if current_char_count + item_size <= max_chars_per_batch:
-                    current_batch.append(item)
-                    current_char_count += item_size
+                try:
+                    item = random.choice(data)
+                    data.remove(item)
+                    item_json = json.dumps(item)
+                    item_size = len(item_json)
+                    if current_char_count + item_size <= max_chars_per_batch:
+                        current_batch.append(item)
+                        current_char_count += item_size
+                except Exception as e:
+                    logging.error(f"Error in main loop: {e}")
             if current_batch:
                 await send_batch_async(current_batch, batch_number, session, weaviate_url)
                 batch_number += 1
